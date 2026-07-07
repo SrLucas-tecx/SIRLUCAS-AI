a30755fba43c7b3f1207aee52e5548ac3fba737f
Juan <guirdvj@gmail.com>
2026-07-07 14:27:21 -0600

    reorganize toda la carpeta

---
+++ SIRLUCASIA/app/main.py
@@ -0,0 +1,17 @@
+class Assistant:
+    def __init__(self):
+        self.name = "SIRLUCAS AI"
+        self.version = "0.1"
+
+    def start(self):
+        print("=" * 50)
+        print(f"{self.name} version {self.version}")
+    print("=" * 50)
+    print("Que onda pa??")
+    print("MI NOMBRE ES SIR LUCAS:)")
+    print("estoy iniciando t tranca")
+    print ("="* 50)
+if __name__ == "__main__":
+    assistant = Assistant()
+    assistant.start()
+    
---
+++ SIRLUCASIA/launcher.py
@@ -0,0 +1,10 @@
+from SIRLUCASIA.app.main import Assistant
+
+
+def main():
+    assistant = Assistant()
+    assistant.start()
+
+
+if __name__ == "__main__":
+    main()
